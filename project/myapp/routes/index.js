dbeb885c58aba97eeac781f7f64f113472eb0361
Tran Nhut Kha <trannhutkha123456@gmail.com>
2019-10-28 22:32:21 +0700

    tao project

---
+++ project/myapp/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/l', function(req, res, next) {
+  res.render('index1', { title: 'kha' });
+});
+
+module.exports = router;
